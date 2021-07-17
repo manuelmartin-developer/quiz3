3af978061391297b19c7adff9fa05985a08a19d5
Tamara <tamaragmartin@gmail.com>
2021-07-17 11:36:55 +0200

    added import-export

---
+++ preguntas.js
@@ -0,0 +1,36 @@
+const preguntas = [
+
+    {
+        question: "¿De qué color están pintadas las paredes del apartamento de Mónica?",
+        correctAnswer: ["Violeta"],
+        incorrectAnswers: ["Blanco", "Amarillo", "Rosa"]
+    },
+    {
+        question: "¿Cómo se llamaba el mono de Ross?",
+        correctAnswer: ["Marcel"],
+        incorrectAnswers: ["Ben", "Gunther", "Paolo"]
+    },
+    {
+        question: "¿Cómo se llama la hermana gemela de Phoebe?",
+        correctAnswer: ["Ursula"],
+        incorrectAnswers: ["Lisa", "Janice", "Regina"]
+    },
+    {
+        question: "¿Cuál es el segundo nombre de Rachel?",
+        correctAnswer: ["Karen"],
+        incorrectAnswers: ["Jill", "Emily", "Muriel"]
+    },
+    {
+        question: "¿Cuál era el título de la telenovela protagonizada por Joey?",
+        correctAnswer: ["Los días de nuestra vida"],
+        incorrectAnswers: ["Amar es para siempre", "A flor de piel", "Los años felices"]
+    },
+    {
+        question: "¿Qué festividad odia Chandler?",
+        correctAnswer: ["Acción de gracias"],
+        incorrectAnswers: ["Cumpleaños", "Navidad", "Halloween"]
+    },
+];
+
+
+export {preguntas};
--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-const preguntas = [
-
-    {
-        question: "¿De qué color están pintadas las paredes del apartamento de Mónica?",
-        correctAnswer: ["Violeta"],
-        incorrectAnswers: ["Blanco", "Amarillo", "Rosa"]
-    },
-    {
-        question: "¿Cómo se llamaba el mono de Ross?",
-        correctAnswer: ["Marcel"],
-        incorrectAnswers: ["Ben", "Gunther", "Paolo"]
-    },
-    {
-        question: "¿Cómo se llama la hermana gemela de Phoebe?",
-        correctAnswer: ["Ursula"],
-        incorrectAnswers: ["Lisa", "Janice", "Regina"]
-    },
-    {
-        question: "¿Cuál es el segundo nombre de Rachel?",
-        correctAnswer: ["Karen"],
-        incorrectAnswers: ["Jill", "Emily", "Muriel"]
-    },
-    {
-        question: "¿Cuál era el título de la telenovela protagonizada por Joey?",
-        correctAnswer: ["Los días de nuestra vida"],
-        incorrectAnswers: ["Amar es para siempre", "A flor de piel", "Los años felices"]
-    },
-    {
-        question: "¿Qué festividad odia Chandler?",
-        correctAnswer: ["Acción de gracias"],
-        incorrectAnswers: ["Cumpleaños", "Navidad", "Halloween"]
-    },
-];
+import { preguntas } from "./preguntas.js";
 
 let pregunta = document.querySelector("#enunciado");
 
@@ -87,34 +55,6 @@ function hideBtn() {
 }
 
 
-/*
 
-let button = document.querySelector("#btnindex");
-button.addEventListener("click", function() {
-    let correcto=0;
-    
-    let q1 = document.quiz.question1.value;
-    let q2 = document.quiz.question2.value;
-    let q3 = document.quiz.question3.value;
-    let q4 = document.quiz.question4.value;
-    let q5 = document.quiz.question5.value;
 
-    let result = document.getElementById("result");
-    let quiz = document.getElementById("quiz");
 
-    if (q1 == "violeta") {correcto++}
-    if (q2 == "marcel") {correcto++}
-    if (q3 == "karen") {correcto++}
-    if (q4 == "ursula") {correcto++}
-    if (q5 == "gracias") {correcto++}
-    
-    quiz.style.display="none";
-
-    if (correcto<=3) { 
-        result.textContent = `Su resultado es ${correcto} sobre 5. Pruebe otra vez!`
-    } else {
-        result.textContent = `Su resultado es ${correcto} sobre 5. Bien hecho!`
-    }  
-  }
-
-*/
